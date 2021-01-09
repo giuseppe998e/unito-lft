163c6f04283d2700b6cc8bad7cd6bd06b5bc7222
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-01-09 22:53:36 +0100

    Add Section05 Ex03

---
+++ sect05/ex03/source.pas
@@ -0,0 +1,16 @@
+(do
+	(= x 10)
+	(while 
+		(>= x (+ 1 0))
+		(do
+			(= x (- x 1))
+			(cond 
+				(> x 4)
+				(print x)
+				(else 
+					(print (+ x 100))
+				)
+			)
+		)
+	)
+)
---
+++ sect05/ex03/src/SymbolTable.java
@@ -0,0 +1,29 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class SymbolTable {
+  private final Map<String, Integer> offsetMap;
+  private int address;
+
+	public SymbolTable() {
+		this.offsetMap = new HashMap<>();
+    address = 0;
+  }
+
+  public int insert(String s) {
+    return insert(s, this.address++);
+  }
+
+  public int insert(String s, int _address) {
+    if (offsetMap.containsValue(_address)) {
+      throw new IllegalArgumentException("Reference to a memory location already occupied by another variable");
+    }
+    
+    offsetMap.put(s, _address);
+    return _address;
+  }
+
+  public int lookupAddress(String s) {
+    return offsetMap.getOrDefault(s, -1);
+  }
+}
